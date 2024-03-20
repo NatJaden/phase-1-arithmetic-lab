bb7df889fafe2b5c9b1951f4fde44be7f7415314
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-03-20 14:24:21 +0300

    [Compulsory] Review: Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+var num1 = 31;
+var num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+const numbers = [5, 10, 15, 20, 19];
+const max = Math.max(...numbers);
+console.log(maxNumber);
+
+const num3 = 24;
+const num4 = 6;
+let mod = num3 / num4;
